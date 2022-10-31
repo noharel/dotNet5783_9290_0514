88e13bc34a832d793721f64d9aaf7874c03c7fca
‘Talel <taleltov@g.jct.ac.il>
2022-10-31 02:44:11 +0200

    trial talel

---
+++ Stage0/Program0514 .cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        static partial void Welcome0514()
+        {
+            Console.WriteLine("I am also here dw");
+        }
+    }
+}

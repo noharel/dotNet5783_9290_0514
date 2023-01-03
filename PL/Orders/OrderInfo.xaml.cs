10538e74b7b6a9e01a4be649b6b45b6deff801b9
talelginsberg@gmail.com <talelginsberg@gmail.com>
2023-01-03 12:17:25 +0200

    sync

--- PL/Orders/OrderInfo.xaml.cs
+++ PL/Orders/OrderInfo.xaml.cs
@@ -19,9 +19,36 @@ namespace PL.Orders
     /// </summary>
     public partial class OrderInfo : Window
     {
-        public OrderInfo()
+        BlApi.IBl? bl = BlApi.Factory.Get(); // get bl from factory
+
+        public OrderInfo(int x = 0)
         {
             InitializeComponent();
+            id.Text =x.ToString();
+            BO.Order order = bl.Order.OrderInfo(x);
+            id.Text = x.ToString();
+            name.Text = order.CustomerName;
+            address.Text = order.CustomerAddress;
+            email.Text = order.CustomerEmail;
+            orderDate.Text = order.OrderDate.ToString();
+            if (order.Status!=BO.OrderStatus.Ordered)
+            {
+                shipDate.Text = order.ShipDate.ToString();
+                if (order.Status == BO.OrderStatus.Arrived)
+                    arrivalDate.Text = order.DeliveryrDate.ToString();
+                else
+                {
+                    arrivalDate.Visibility = Visibility.Collapsed;
+                    labelArrivalDate.Visibility = Visibility.Collapsed;
+                }
+            }
+            else
+            {
+                shipDate.Visibility = Visibility.Collapsed;
+                labelShipDate.Visibility = Visibility.Collapsed;
+            }
+            status.Text = order.Status.ToString();
+            totalPrice.Text = order.TotalPrice.ToString();
         }
     }
 }

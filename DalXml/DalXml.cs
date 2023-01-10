1f1c12aedb864ad0f5bf8d77f5cfec172297bd8c
noharel <noharel@g.jct.ac.il>
2023-01-10 23:06:47 +0200

    start Xml

---
+++ DalXml/DalXml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DO;
+using DalApi;
+using System.Diagnostics;
+
+
+namespace Dal;
+
+sealed internal class DalXml:IDal
+{
+    public static IDal Instance { get; } = new DalXml(); // singelton
+
+    private DalXml() { }
+    public IOrder Order { get; } = new Dal.Order();
+    public IProduct Product { get; } = new Dal.Product();
+    public IOrderItem OrderItem { get; } = new Dal.OrderItem();
+}
---
+++ DalXml/Order.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using DalApi;
+using DO;
+
+namespace Dal;
+
+internal class Order : IOrder
+{
+    DalApi.IDal Dal = DalApi.Factory.Get(); //DalList object Type
+    public int Add(DO.Order item)
+    {
+        int id = Dal.Order.Add(item);
+        return Dal.Order.Add(item);
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<DO.Order?> GetAll(Func<DO.Order?, bool>? filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<DO.Order?> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public DO.Order GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public DO.Order? GetById(Func<DO.Order?, bool>? filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(DO.Order item)
+    {
+        throw new NotImplementedException();
+    }
+}

0cfb7f718b7d0173a54ef94551e2206494e49300
talelginsberg@gmail.com <talelginsberg@gmail.com>
2023-01-15 14:06:50 +0200

    sync

--- DalFacade/DO/Product.cs
+++ DalFacade/DO/Product.cs
@@ -6,13 +6,13 @@ public struct Product
     
     public string? Name { get; set; } // Name of the product
     
-    public double Price { get; set; } // price of the product
+    public double? Price { get; set; } // price of the product
     
     public Category? Category { get; set; } // Category of the product
     
-    public int InStock { get; set ; } // if the product is in stock
+    public int? InStock { get; set ; } // if the product is in stock
     
-    public bool IsDeleted { get; set ; } // If the order item was deleted 
+    public bool? IsDeleted { get; set ; } // If the order item was deleted 
     public override string ToString() // for printing
     {
         return ToolStringClass.ToStringProperty(this);
